b6d6e277698c1a5e2874c972b6f06417367410e7
omar0200 <omarminato17@gmail.com>
2026-02-25 23:16:29 +0200

    Question 3

--- G-NET-9-OOP01/Program.cs
+++ G-NET-9-OOP01/Program.cs
@@ -18,38 +18,41 @@
 
         {
             #region Question 1
-            //struct
+            ////struct
 
-            X x1 = new X();
-            x1.a = 10;
-            x1.b = 20;
-            X x2 = new X();
-            x2 = x1;
-            x2.a = 30;
-            
-            //class
-            Y y1 = new Y();
-            y1.a = 10;
-            y1.b = 20;
-            Y y2 = new Y();
-            y2 = y1;
-            y2.a = 30;
-          
+            //X x1 = new X();
+            //x1.a = 10;
+            //x1.b = 20;
+            //X x2 = new X();
+            //x2 = x1;
+            //x2.a = 30;
 
-            Console.WriteLine($"x1: {x1.a}, {x1.b}");
-            Console.WriteLine($"x2: {x2.a}, {x2.b}");
-            Console.WriteLine($"y1: {y1.a}, {y1.b}");
-            Console.WriteLine($"y2: {y2.a}, {y2.b}");
+            ////class
+            //Y y1 = new Y();
+            //y1.a = 10;
+            //y1.b = 20;
+            //Y y2 = new Y();
+            //y2 = y1;
+            //y2.a = 30;
+
+
+            //Console.WriteLine($"x1: {x1.a}, {x1.b}");
+            //Console.WriteLine($"x2: {x2.a}, {x2.b}");
+            //Console.WriteLine($"y1: {y1.a}, {y1.b}");
+            //Console.WriteLine($"y2: {y2.a}, {y2.b}");
             //struct assign values meanwhile class assign reference,
             //so when we change the value of x2.a it does not affect x1.a but when we change the value of y2.a it affects y1.a because they are reference type.
             #endregion
 
             #region Question 2
-            Y y3 = new Y(); 
-            Console.WriteLine($"y3: {y3.a}, {y3.b}, {y3.c} ");
+            //Y y3 = new Y(); 
+            //Console.WriteLine($"y3: {y3.a}, {y3.b}, {y3.c} ");
             //we cannot access the private member c of class Y, so we will get an error if we try to access it.
             #endregion
 
+            #region Question 3
+            //open new project as class library and add reference to it in this project, then we can create an instance of the class in the class library and access its members.
+            #endregion
         }
     }
 }
